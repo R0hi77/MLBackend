9287f3f5e02d070df84eef3ba9a83128a7c5ebc8
R0hi77 <pyawinbe@gmail.com>
2025-12-02 15:54:48 +0000

    use new broker domain

--- app/configurations/config.py
+++ app/configurations/config.py
@@ -12,7 +12,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
 
-    MQTT_BROKER_URL = os.getenv('MQTT_BROKER_URL', '107.23.193.231')
+    MQTT_BROKER_URL = os.getenv('MQTT_BROKER_URL', 'broker.peterzzburg.online')
     MQTT_BROKER_PORT = int(os.getenv('MQTT_BROKER_PORT', 8884))
     # MQTT_USERNAME = os.getenv('MQTT_USERNAME', 'iotNodeUser')
     # MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', 'He11owor!d')
